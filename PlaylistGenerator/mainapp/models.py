55520f2f99f5652aad6db7bdd90968d7c78d1360
KarinaZalevska <s0beit72bt@gmail.com>
2026-01-02 20:26:09 +0200

    add normal model

--- PlaylistGenerator/mainapp/models.py
+++ PlaylistGenerator/mainapp/models.py
@@ -7,11 +7,11 @@ class SpotifyToken(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="spotify_token")
     access_token = models.CharField(max_length=300)
     refresh_token = models.CharField(max_length=300)
-    expires_at = models.DateTimeField(default=timezone.now)
+    expires_at = models.DateTimeField()
     token_added_at = models.DateTimeField(auto_now_add=True)
 
-    def is_expired(self):
+    def is_expired(self) -> bool:
         return self.expires_at <= timezone.now()
 
     def __str__(self):
-        return f"Spotify token for {self.user.username}"
+        return f"SpotifyToken(user={self.user.username})"
